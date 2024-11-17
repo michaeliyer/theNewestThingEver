60cec49e53ee89750bdaf79661ea59af4c09eb35
IYERM <michaeliyer@gmail.com>
2024-11-16 22:45:11 -0500

    added an h2, some font color, a console.log message

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const movieOne = "The VVitch";
-console.log(movieOne)
+console.log(movieOne);
 const movieTwo = "Hell Watcher";
-console.log(movieTwo)
+console.log(movieTwo);
+const movieThree = "Dog Man";
+console.log(movieThree);
