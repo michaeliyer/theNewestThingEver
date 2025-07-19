abb1afd79d20a0a450bc0a7180a01e6009df30ae
Michael Iyer <your-email@example.com>
2025-07-19 00:08:40 -0400

    Bs commit

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ const movieThree = "Dog Man";
 console.log(movieThree);
 const movieFour = "Dog Man II";
 console.log(movieThree, 5 * 4, "Great job, Phillie! ", "You know," + movieOne + " and " + movieTwo + " are pretty cool flicks, man!", "so let's " + 30 + 30 + 81/9 + "!");
+console.log(movieThree, 5 * 4, "Great job, Phillie");
+
